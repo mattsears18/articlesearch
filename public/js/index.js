3cab2e27e2e4b7ae4ab95687f8652190db7c2e5f
Matt Sears <mattsears@Matts-MacBook-Pro.local>
2016-12-31 15:01:48 -0500

    file upload form

--- public/js/index.js
+++ public/js/index.js
@@ -6,4 +6,34 @@ $(function() {
   $('#articleUploadFile').change(function() {
     $(this).closest('form').submit();
   });
+
+  var progress = $('.progress');
+  var bar = $('.bar');
+  var percent = $('.percent');
+  var status = $('#status');
+
+  $('form').ajaxForm({
+      beforeSend: function() {
+        status.empty();
+        var percentVal = '0%';
+        bar.width(percentVal)
+        bar.html(percentVal);
+        progress.slideDown();
+      },
+      uploadProgress: function(event, position, total, percentComplete) {
+        var percentVal = percentComplete + '%';
+        bar.width(percentVal)
+        bar.html(percentVal);
+        console.log(percentVal, position, total);
+      },
+      success: function() {
+        var percentVal = '100%';
+        bar.width(percentVal)
+        bar.html(percentVal);
+      },
+  	complete: function(xhr) {
+  		status.html(xhr.responseText);
+      progress.slideUp();
+  	}
+  });
 });
